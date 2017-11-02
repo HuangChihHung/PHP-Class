f9f8ca5a9023e4f2504c78c987b726d986f75b93
HuangChihHung <pk33386@yahoo.com.tw>
2017-11-02 10:00:32 +0800

    把 動物共同會做的事 放在父類別

--- AllClass/Animal.php
+++ AllClass/Animal.php
@@ -6,16 +6,11 @@
 //4. 可以取得物種名稱
 //5. 可以從外部得到食物
 
-
-
-
-
 class Animal
 {
-
-
-    private $animalType ="default animal Type";
+    private $animalType = "default animal Type";//若沒有呼叫父類別的建構子, 會印出這個
     protected $position;
+    protected $foodName;
 
     public function __construct($animalTypeFromOut) {
         $this->animalType = $animalTypeFromOut;
@@ -27,6 +22,21 @@ class Animal
     }
 
     public function getFood($foodName) {
+        $this->foodName = $foodName;
         echo "得到食物: $foodName<br>";
+        //判斷要不要吃
+        $this->eat();
+    }
+
+    protected function eat() {
+        echo "吃".$this->foodName."<br>";
+
+        $this->digest();
+    }
+
+    private function digest() {
+        echo "消化".$this->foodName."<br>";
     }
 }
+
+//$animal = new Animal("test");
--- AllClass/Cat.php
+++ AllClass/Cat.php
@@ -1,32 +1,24 @@
 <?php
 
-
-
 include_once "Animal.php";
 
 class Cat extends Animal
 {
-
-
     private $petName;
 
-
-
-
-    public function __construct($petNameFromOut)
-    {
-        parent::__construct("貓");
+    public function __construct($petNameFromOut) {
+        parent::__construct("貓");//需要呼叫父類別初始化的方法, 否則父類別不會做他的建構子的初始動作
         $this->petName = $petNameFromOut;
+        $this->position;
     }
 
-
-    function getPetName(){
-
-        return $this->getPetName();
-
+    public function getPetName() {
+//        $this->getAnimalType();//測試 protected 方法
+        return $this->petName;
     }
-
 }
 
 $cat = new Cat("Kitty");
-echo $cat->getAnimalType();
+echo $cat->getAnimalType();//繼承後可以直接使用父類別的 public 方法
+
+$cat->getFood("小魚");
